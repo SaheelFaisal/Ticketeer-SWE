d9e2572a2d0bf37917b1a1e1b57af52efecbe3a0
Saheel Faisal <saheelfaisal@outlook.com>
2025-04-20 12:09:22 -0500

    added movie model

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Movie(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration_minutes = models.PositiveIntegerField()
+    genre = models.CharField(max_length=100)
+    language = models.CharField(max_length=50)
+    poster_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
