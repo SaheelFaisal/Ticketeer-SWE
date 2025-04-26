47c5778a95b34936ea0ce4a014d59dcd96f8e90d
Saheel Faisal <saheelfaisal@outlook.com>
2025-04-26 00:16:17 -0500

    added theatre and show models

---
+++ backend/theatres/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from movies.models import Movie
+
+class Theatre(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.location})"
+
+class Show(models.Model):
+    theatre = models.ForeignKey(Theatre, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    language = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.movie.title} at {self.theatre.name} on {self.start_time.strftime('%Y-%m-%d %H:%M')}"
