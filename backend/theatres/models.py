3dd94c08471fe7a118368a32d6f4fabde49f448b
Saheel Faisal <saheelfaisal@outlook.com>
2025-04-26 00:29:30 -0500

    modified show model, added price

--- backend/theatres/models.py
+++ backend/theatres/models.py
@@ -13,6 +13,7 @@ class Show(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     start_time = models.DateTimeField()
     language = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=10.00)
 
     def __str__(self):
         return f"{self.movie.title} at {self.theatre.name} on {self.start_time.strftime('%Y-%m-%d %H:%M')}"
